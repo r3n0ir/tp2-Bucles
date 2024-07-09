15b239979b0b8173e7eecade50f6059328a906f7
Elian Miranda <elianmir@outlook.com>
2024-07-08 22:30:30 -0300

    creados todos los archivos

---
+++ js/ejercicio-1.js
@@ -0,0 +1,7 @@
+const edad = parseInt(prompt("Ingrese su edad: "))
+
+if (edad >= 18) {
+    document.write("Usted ya puede conducir.")
+} else {
+    document.write("Usted es menor, no puede conducir")
+}
